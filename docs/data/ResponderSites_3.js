73827dfa2b44b0a1a3c62ea83921dbcef7014812
Rishan Rastogi <rastogirishan@gmail.com>
2026-09-05 17:32:51 +0800

    Add responder sites layer to web map, fix road legend color, remove boundary.json

---
+++ docs/data/ResponderSites_3.js
@@ -0,0 +1,22 @@
+var json_ResponderSites_3 = {
+"type": "FeatureCollection",
+"name": "ResponderSites",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": 1, "rank": 1, "new_covered": 89, "cumulative_covered": 89, "cumulative_pct": 19.2 }, "geometry": { "type": "Point", "coordinates": [ 36.224908658178876, -3.352454328101705 ] } },
+{ "type": "Feature", "properties": { "fid": 2, "rank": 2, "new_covered": 52, "cumulative_covered": 141, "cumulative_pct": 30.4 }, "geometry": { "type": "Point", "coordinates": [ 36.236123022952242, -3.403342966701659 ] } },
+{ "type": "Feature", "properties": { "fid": 3, "rank": 3, "new_covered": 52, "cumulative_covered": 193, "cumulative_pct": 41.6 }, "geometry": { "type": "Point", "coordinates": [ 36.309619883529322, -3.400542517442191 ] } },
+{ "type": "Feature", "properties": { "fid": 4, "rank": 4, "new_covered": 44, "cumulative_covered": 237, "cumulative_pct": 51.1 }, "geometry": { "type": "Point", "coordinates": [ 36.178576407291857, -3.417893329383134 ] } },
+{ "type": "Feature", "properties": { "fid": 5, "rank": 5, "new_covered": 39, "cumulative_covered": 276, "cumulative_pct": 59.5 }, "geometry": { "type": "Point", "coordinates": [ 36.202206959876122, -3.451247107278266 ] } },
+{ "type": "Feature", "properties": { "fid": 6, "rank": 6, "new_covered": 38, "cumulative_covered": 314, "cumulative_pct": 67.7 }, "geometry": { "type": "Point", "coordinates": [ 36.331438073092045, -3.351511123870087 ] } },
+{ "type": "Feature", "properties": { "fid": 7, "rank": 7, "new_covered": 30, "cumulative_covered": 344, "cumulative_pct": 74.1 }, "geometry": { "type": "Point", "coordinates": [ 36.166402624270042, -3.369672994339548 ] } },
+{ "type": "Feature", "properties": { "fid": 8, "rank": 8, "new_covered": 23, "cumulative_covered": 367, "cumulative_pct": 79.1 }, "geometry": { "type": "Point", "coordinates": [ 36.259399980094891, -3.360250456729618 ] } },
+{ "type": "Feature", "properties": { "fid": 9, "rank": 9, "new_covered": 16, "cumulative_covered": 383, "cumulative_pct": 82.5 }, "geometry": { "type": "Point", "coordinates": [ 36.345274855784183, -3.415973851628203 ] } },
+{ "type": "Feature", "properties": { "fid": 10, "rank": 10, "new_covered": 15, "cumulative_covered": 398, "cumulative_pct": 85.8 }, "geometry": { "type": "Point", "coordinates": [ 36.243487128622625, -3.442311624888208 ] } },
+{ "type": "Feature", "properties": { "fid": 11, "rank": 11, "new_covered": 14, "cumulative_covered": 412, "cumulative_pct": 88.8 }, "geometry": { "type": "Point", "coordinates": [ 36.215969270363289, -3.33945836879607 ] } },
+{ "type": "Feature", "properties": { "fid": 12, "rank": 12, "new_covered": 12, "cumulative_covered": 424, "cumulative_pct": 91.4 }, "geometry": { "type": "Point", "coordinates": [ 36.190656726525155, -3.390329356019171 ] } },
+{ "type": "Feature", "properties": { "fid": 13, "rank": 13, "new_covered": 7, "cumulative_covered": 431, "cumulative_pct": 92.9 }, "geometry": { "type": "Point", "coordinates": [ 36.238193752091881, -3.381839064775448 ] } },
+{ "type": "Feature", "properties": { "fid": 14, "rank": 14, "new_covered": 7, "cumulative_covered": 438, "cumulative_pct": 94.4 }, "geometry": { "type": "Point", "coordinates": [ 36.248534371395799, -3.313656539802836 ] } },
+{ "type": "Feature", "properties": { "fid": 15, "rank": 15, "new_covered": 7, "cumulative_covered": 445, "cumulative_pct": 95.9 }, "geometry": { "type": "Point", "coordinates": [ 36.29445, -3.31263 ] } }
+]
+}
